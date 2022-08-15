91cc656fc5d8c4ba9f2c006b49c930c20405c8da
Projeto_SIGA <special.vips@gmail.com>
2022-08-15 17:34:11 -0300

    Comit-Estudos-Python

---
+++ exercicio.py
@@ -0,0 +1,8 @@
+nome = input ('Qual é o seu nome? ')
+peso = input ('Informe seu peso: ')
+dia = input ('Informe o seu dia de Nascimento: ')
+mes = input ('Informe o seu mes de Nascimento: ')
+ano = input ('Informe o ano do seu Nascumento: ')
+print (f'Seu nome é {nome}')
+print (f'Seu peso é {peso}')
+print (f'Você nasceu no dia {dia} de {mes} de {ano}.')
