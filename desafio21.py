6293016b660090ca9708a1b05a5e63279606c2cd
Projeto_SIGA <special.vips@gmail.com>
2022-08-23 16:00:46 -0300

    Novos Desafios

--- desafio21.py
+++ desafio21.py
@@ -3,7 +3,7 @@
 import pygame
 
 pygame.mixer.init()
+pygame.init()
 pygame.mixer.music.load('desafio21.mp3')
 pygame.mixer.music.play()
-input()
 pygame.event.wait()
---
+++ desafio26.py
@@ -0,0 +1,4 @@
+# Faça uma programa que leia uma frase pelo teclado e mostre:
+# Quantas vezes aparece a letra "A"
+# Em que posição ela aparece a primeira vez
+# Em posicão ela aparece a última vez
