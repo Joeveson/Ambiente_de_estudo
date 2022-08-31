49661ccfe6b3c4224e2aae5b4e9a9b882932cfed
Projeto_SIGA <special.vips@gmail.com>
2022-08-31 16:07:59 -0300

    Aula 11 - Cores no TERMINAL

---
+++ aula11.py
@@ -0,0 +1,14 @@
+# AULA 11 - CORES NO TERMINAL
+
+# \033[0;30;41m
+# \033[4;33;44m
+# \033[1;35;43m
+# \033[30;42m
+# \033[m
+# \033[7;30m
+
+# print(f'\033[7;41mHello World\033[m')
+
+a = 3
+b = 5
+print (f'Os valores são \033[32m{a}\033[m e \033[31m{b}\033[m!!!')
