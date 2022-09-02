05ab50d71799a23dcbf4066ddaff25f06538982f
Projeto_SIGA <special.vips@gmail.com>
2022-09-02 15:50:17 -0300

    Exercicios 46 a 56 - PARA FAZER

---
+++ aula013.py
@@ -0,0 +1,41 @@
+# for c in range(1, 20):
+#     print('Hello World')
+# print('FIM!!!')
+
+# for c in range(1, 20):
+#     print(c)
+# print('FIM!!!')
+
+# for c in range(6, 0, -1):
+#     print(c)
+# print('FIM!!!')
+
+# for c in range(0, 7):
+#     print(c)
+# print('FIM!!!')
+
+# for c in range(0, 7, 2):
+#     print(c)
+# print('FIM!!!')
+
+# n = int(input('Digite um número: '))
+# for c in range(0, n+1):
+#     print(c)
+# print('FIM')
+
+# i = int(input('Início: '))
+# f = int(input('Fim: '))
+# p = int(input('Pula: '))
+# for c in range(i, f+1, p):
+#     print(c)
+# print('FIM')
+
+# for c in range(0,3):
+#     n = int(input('Digite um número: '))
+# print('FIM')
+
+s = 0
+for c in range(0,3):
+    n = int(input('Digite um número: '))
+    s = s + n
+print(f'O somatório de todos os valores foi {s}.')
---
+++ desafio53.py
@@ -0,0 +1,4 @@
+3 # Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo desconsiderando os espaços.
+
+# FRASE QUE VOCÊ PODE LER DE TRÁS PRA FRENTE - DA FRENTE PRA TRÁS
+# EX: APOS A SOPA / A SACADA DA CASA / A TORRE DA DERROTA / O LOBO AMA O BOLO / ANOTARAM A DATA DA MARATONA 
