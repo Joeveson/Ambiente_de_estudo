bd82e1ce434eaa771dfbc09da74c53080e0930e1
Joeveson <special.vips@gmail.com>
2022-09-05 21:19:05 -0300

    Desafios finalizado

--- desafio56.py
+++ desafio56.py
@@ -5,34 +5,34 @@
 # Quantas mulheres têm menos de 20 anos.
 
 soma = 0
-count_m = 0
 media = 0
+qtd_mulheres = 0
 maior = 0
-homem_velho = ''
+h_mais_velho = 0
 
 for c in range(1, 5):
     nome = (input('Informe o Nome: '))
-    sexo = int(input('Informe o Sexo [1] Masculino [2] Feminino: ').upper())
+    sexo = str(input('Informe o Sexo [M] Masculino [F] Feminino: ').upper())
     idade = int(input('Informe a Idade: '))
 
+    soma = soma + idade
+    media = soma / idade
 
+    if sexo == 'M' and idade > maior:
+        maior = idade
+        h_mais_velho = nome
+    print(h_mais_velho) 
 
-#     soma = soma + idade
-#     media = soma / idade
-
-#     if sexo == "1" and idade > maior:
-#         maior = idade
-#         homem_velho = nome
+    if sexo == 'F' or idade <20:
+        qtd_mulheres = qtd_mulheres +1
+    print (qtd_mulheres)
     
-#     if sexo == "2" and idade <20:
-#         count_m = count_m +1
+print (f'Média das idade é de {media:.2f}.')
+print (f'Nome do homem mais velho é {h_mais_velho}.')
 
-# print (f'Média das idade é de {media:.2f}.')
-# print (f'Nome do homem mais velho é {homem_velho}.')
+if qtd_mulheres == 0:
+    print('Não tem mulheres no grupo! ')
 
-# if count_m == 0:
-#     print('Não tem mulheres no grupo! ')
-
-# else:
-#     print(f'Ao todo {count_m} mulheres com menor de 20 anos.')
+else:
+    print(f'Ao todo {qtd_mulheres} mulheres com menos de 20 anos.')
 
