a8b183d724709037f2f5d2d428487fbc1a1d773f
Projeto_SIGA <special.vips@gmail.com>
2022-09-08 17:53:14 -0300

    Resolvidas

--- desafio64.py
+++ desafio64.py
@@ -1,1 +1,15 @@
 # Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. no Final, mostre quantos números foram digitados e qual foi a soma entre eles. (Desconsiderar o Flag)
+
+from itertools import count
+
+
+n = 1
+cont = 0
+cont_n = 0
+
+while n != 999:
+    n = int (input('Informe um número: '))
+    cont = n + cont
+    cont_n = cont_n +1
+
+print(f'Foram informados {cont_n} números e a soma deles é {cont}.')
