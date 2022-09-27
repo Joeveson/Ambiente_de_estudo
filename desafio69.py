76126e937396feae1329a677341bb5d6e737897a
Projeto_SIGA <special.vips@gmail.com>
2022-09-27 17:53:54 -0300

    Resolvendo questão 69

--- desafio69.py
+++ desafio69.py
@@ -3,3 +3,23 @@
 # B- Quantos homens foram cadastrados. 
 # C- Quantas mulheres tem menos de 20 anos.
 
+cont = 0
+while True:
+    idade = int(input('Informe sua idade: '))
+    print("""Informe seu sexo:  
+    [F] = Feminino 
+    [M] = Masculino
+    """)
+    sexo = str(input('Informe seu sexo: ').upper())
+    if idade > 18:
+        cont = cont +1
+        print (f'Existe {cont} pessoa(s) com mais de 18 anos.')
+    
+    if sexo == 'H':
+        cont = cont +1
+        print (f'Foram cadastrados {cont} Homen(s).')
+ 
+    if sexo == 'M':
+        idade > 20
+        cont = cont +1
+        print (f'Existe {cont} mulhere(s) com menos de 20 anos.')
