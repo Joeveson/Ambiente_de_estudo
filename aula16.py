296032fb0cee5f5ef4fd60b6d4d6919bb7890c7e
Projeto_SIGA <special.vips@gmail.com>
2022-11-25 14:15:29 -0300

    Desafios 72 ao 77

--- aula16.py
+++ aula16.py
@@ -14,3 +14,12 @@ print('Comi pra caramba! :)')
 
 
 # print(sorted(lanche))
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+print (c)
+
+# print (c.count(5)) MOSTRA QUANTAS VEZES APARECEU O NÚMERO 5 
+# print (c.index(8)) MOSTRA A POSIÇÃO QUE APRESENTA O NÚMERO DEFINIDO
+
