4551103f7613fd19683a4e0a3ad4e65e72b07a02
Projeto_SIGA <special.vips@gmail.com>
2022-12-05 13:07:58 -0300

    Respotas do desafio 76 / 77

--- desafio76.py
+++ desafio76.py
@@ -1,2 +1,19 @@
 # CRIE UM PROGRAMA QUE TENHA UMA TUPLA ÚNICA COM NOMES DE PRODUTOS E SEUS RESPECTIVOS PREÇOS, NA SEQUÊNCIA. 
 # NO FINAL, MOSTRE UMA LISTAGEM DE PREÇOS, ORGANIZANDO OS DADOS EM FORMA TABULAR.
+
+
+
+print ('-'*42)
+print ('            LISTAGEM DE PREÇOS            ')
+print ('-'*42)
+
+lista = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livros', 34.90,)
+
+
+for pos, item in enumerate (lista):
+    if pos % 2 == 0:
+        print (f'{item:.<32}' ,end='')
+    else:
+        print(f'R$ {item:7.2f}')
+
+print ('-'*42)
--- desafio77.py
+++ desafio77.py
@@ -1,1 +1,13 @@
 # CRIE UM PROGRAMA QUE TENHA UM TUPLA COM VÁRIAS PALAVRAS (NÃO USAR ACENTOS). DEPOIS DISSO, VOCÊ DEVE MOSTRAR, PARA CADA PALAVRA, QUAIS SÃO AS SUAS VOGAIS.
+
+lista = ('AVIAO', 'ELEFANTE', 'BOLA', 'PEIXE', 'FUTEBOL', 'BELISCAO', 'PAPEL', 'BASQUETE', 'CONTROLE', 'TRISTE', 'GATO', 'GOLFE', 'TESOURA', 'COLHER', 'PULAR', 'GALINHA', 'SAPO', 'ESPIRITO', 'MARTELO')
+vogais = 'AEIOU'
+
+for palavra in lista:
+    print (f'Na palavra {palavra} temos ' ,end='')
+    for vogal in palavra:
+    
+        if vogal in vogais:
+            print (f'{vogal} ' ,end='')
+    
+    print()
